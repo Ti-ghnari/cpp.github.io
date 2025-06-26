42ea0ee36102cdc7a1b1eb7bab6b970a14461425
Tighnari <13673112567@163.com>
2025-06-26 11:19:09 +0800

    Update 数颜色.cpp

--- 博客用代码/数颜色.cpp
+++ 博客用代码/数颜色.cpp
@@ -1,21 +1,60 @@
 #include<bits/stdc++.h>
-#define N 100010
 #define ll long long
+#define N 233333
+#define M 1000010
 using namespace std;
-void write(int x){
-	if(x<0) putchar('-'),x=-x;
-	if(x>9) write(x/10);
-	putchar(x%10+'0');
+struct Q{
+	int x,y,t,id;
+}qry[N],mdf[N];
+int cntq,cntm;
+int n,m,a[N],ans[N];
+int len,cnt[M],sum;
+bool cmp(Q a,Q b){
+	return a.x/len==b.x/len ? a.y/len==b.y/len ? a.t<b.t : a.y < b.y : a.x<b.x;
 }
-inline int read(){
-	int x=0,f=1;
-	char ch=getchar();
-	while(ch<'0'||ch>'9') {if(ch=='-')f=-1;ch=getchar();}
-	while(ch>='0' && ch<='9') x=x*10+ch-'0',ch=getchar();
-	return x*f;
+inline void add(int x){
+	sum+=!cnt[x]++;
 }
-int main(){
-	
-	
+inline void del(int x){
+	sum-=!--cnt[x];
+}
+inline void upd(int x,int t){
+	if(qry[x].x<=mdf[t].x && mdf[t].x <= qry[x].y){
+		del(a[mdf[t].x]);
+		add(mdf[t].y);
+	}
+	swap(a[mdf[t].x],mdf[t].y);
+}
+signed main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);cout.tie(0);
+	cin>>n>>m;
+	len=pow(n,0.666);
+	for(int i=1;i<=n;i++){
+		cin>>a[i];
+	}
+	for(int i=1;i<=m;i++){
+		char op;int x,y;
+		cin>>op>>x>>y;
+		if(op=='Q'){
+			qry[++cntq]={x,y,cntm,cntq};
+		}else{
+			mdf[++cntm]={x,y,cntm,cntm};
+		}
+	}
+	sort(qry+1,qry+1+cntq,cmp);
+	int l=1,r=0,t=0;
+	for(int i=1;i<=cntq;i++){
+		while(l>qry[i].x) add(a[--l]);
+		while(l<qry[i].x) del(a[l++]);
+		while(r>qry[i].y) del(a[r--]);
+		while(r<qry[i].y) add(a[++r]);
+		while(t<qry[i].t) upd(i,++t);
+		while(t>qry[i].t) upd(i,t--);
+		ans[qry[i].id]=sum;
+	}
+	for(int i=1;i<=cntq;i++){
+		cout << ans[i] << '\n';
+	}
 	return 0;
 }
